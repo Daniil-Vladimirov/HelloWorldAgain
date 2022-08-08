79b0fd82248f3799e66e2b26d22488723e02b6e0
Daniil <porvipop@mail.ru>
2022-08-08 21:10:39 +0300

    Add showToast & countUp functionality

--- app/src/main/java/com/example/helloworldagain/MainActivity.kt
+++ app/src/main/java/com/example/helloworldagain/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.helloworldagain
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount: Int = 0
+    private var countView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        countView = findViewById(R.id.tv_show_count)
 
         Log.d("MainActivity", "Hello World")
     }
@@ -18,5 +24,8 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
     }
 
-    fun countUp(view: View) {}
+    fun countUp(view: View) {
+        mCount++
+        countView?.text = mCount.toString()
+    }
 }
